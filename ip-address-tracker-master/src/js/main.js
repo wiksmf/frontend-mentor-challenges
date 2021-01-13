7158f47fde8c40d515e04369ede857e0e4db052f
wiksmf <wiksmf@gmail.com>
2021-01-13 01:21:10 +0100

    js update

--- ip-address-tracker-master/src/js/main.js
+++ ip-address-tracker-master/src/js/main.js
@@ -2,45 +2,77 @@
 
 const form = document.querySelector('.form');
 const userInput = document.querySelector('.form__input');
+const infoCard = document.querySelector('.card');
 
 const accessToken = config.ACCESS_TOKEN;
+const apiKey = config.API_KEY;
 
-class App {
-  // #mapEvent;
+const map = L.map('map');
+const blackIcon = L.icon({ iconUrl: 'src/img/icon-location.svg' });
 
-  constructor() {
-    // Get user's position
-    this._getPosition();
+// Getting user's position
+const getPosition = async function () {
+  try {
+    const res = await fetch(`https://geo.ipify.org/api/v1?apiKey=${apiKey}`);
+    if (!res.ok) throw new Error('Problem getting location data.');
+
+    const geoData = await res.json();
+
+    loadMap(geoData.location.lat, geoData.location.lng);
+    displayInformation(geoData);
+  } catch (err) {
+    console.error(`⛔ ${err}`);
   }
+};
 
-  _getPosition() {
-    if (navigator.geolocation)
-      navigator.geolocation.getCurrentPosition(
-        this._loadMap.bind(this),
-        console.warn('Could not get your position'),
-      );
-  }
+// Loading Leaflet map
+const loadMap = function (latitude, longitude) {
+  const coords = [latitude, longitude];
 
-  _loadMap(position) {
-    const { latitude, longitude } = position.coords;
+  L.tileLayer(
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: accessToken,
+    },
+  ).addTo(map);
 
-    const coords = [latitude, longitude];
+  map.setView(coords, 13);
+  L.marker(coords, { icon: blackIcon }).addTo(map);
+};
 
-    const map = L.map('map').setView(coords, 13);
+// Updating information card
+const displayInformation = function (geoData) {
+  let html = `
+    <div class="card-box">
+      <h2 class="heading-secondary u-margin-bottom-small">IP ADDRESS</h2>
+      <p class="card-box__info">${geoData.ip}</p>
+    </div>
+    <div class="card-box">
+      <h2 class="heading-secondary u-margin-bottom-small">LOCATION</h2>
+      <p class="card-box__info">${geoData.location.city}, ${geoData.location.country} ${geoData.location.postalCode}</p>
+    </div>
+    <div class="card-box">
+      <h2 class="heading-secondary u-margin-bottom-small">TIMEZONE</h2>
+      <p class="card-box__info">UTC${geoData.location.timezone}</p>
+    </div>
+    <div class="card-box">
+      <h2 class="heading-secondary u-margin-bottom-small">ISP</h2>
+      <p class="card-box__info">${geoData.isp}</p>
+    </div>
+  `;
 
-    L.tileLayer(
-      'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-      {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: accessToken,
-      },
-    ).addTo(map);
-  }
-}
+  infoCard.insertAdjacentHTML('afterbegin', html);
+};
 
-const app = new App();
+const init = function () {
+  getPosition();
+  loadMap();
+};
+
+init();
