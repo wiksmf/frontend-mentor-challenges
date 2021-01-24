bfec6deb5e35bcea77550034ea25a19c49f8aedd
wiksmf <wiksmf@gmail.com>
2021-01-24 18:18:40 +0100

    js update

---
+++ intro-component-with-sign-up-form/src/js/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const form = document.querySelector('.form');
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#pwd');
+
+const regEx = {
+  email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+  pwd: /^[A-Za-z0-9]\w{8,16}$/,
+};
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  !firstName.value ? displayError(firstName) : removeError(firstName);
+  !lastName.value ? displayError(lastName) : removeError(lastName);
+  !regEx.email.test(email.value) ? displayError(email) : removeError(email);
+  !regEx.pwd.test(password.value)
+    ? displayError(password)
+    : removeError(password);
+});
+
+function displayError(input) {
+  input.classList.add('error__input');
+  input.nextElementSibling.classList.remove('u-hidden');
+  input.setAttribute('placeholder', '');
+}
+
+function removeError(input) {
+  input.classList.remove('error__input');
+  input.nextElementSibling.classList.add('u-hidden');
+}
