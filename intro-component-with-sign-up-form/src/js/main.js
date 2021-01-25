deac9d3f00677d1b7fa64865d324443a69d9a639
wiksmf <wiksmf@gmail.com>
2021-01-25 16:05:12 +0100

    js update

--- intro-component-with-sign-up-form/src/js/main.js
+++ intro-component-with-sign-up-form/src/js/main.js
@@ -8,7 +8,7 @@ const password = document.querySelector('#pwd');
 
 const regEx = {
   email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-  pwd: /^[A-Za-z0-9]\w{8,16}$/,
+  pwd: /^[A-Za-z0-9]\w{7,15}$/,
 };
 
 form.addEventListener('submit', e => {
