c8ab61aedf788fb990e57581379c798cadb3729a
wiksmf <wiksmf@gmail.com>
2021-01-28 23:25:18 +0100

    html, css, js update

---
+++ easybank-landing-page/src/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const navigationBtn = document.querySelector('.navigation__btn');
+const navigation = document.querySelector('.navigation__nav');
+
+navigationBtn.addEventListener('click', () => {
+  navigation.classList.toggle('u-hidden');
+  navigation.classList.toggle('navigation__nav--animated-in');
+});
