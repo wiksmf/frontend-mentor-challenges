67d91847ee480edea8c7b6ab3739d90ab9ac1cbf
wiksmf <wiksmf@gmail.com>
2021-04-24 22:35:27 +0200

    html,sass update

--- todo-app-main/src/js/main.js
+++ todo-app-main/src/js/main.js
@@ -2,15 +2,15 @@
 
 const container = document.querySelector('.container');
 const themeSwitch = document.querySelector('.btn-theme');
-const newTodo = document.querySelector('.input__add-todo');
+const newTodo = document.querySelector('.block__add-todo__input');
 const btnAdd = document.querySelector('.btn-add');
 const btnClear = document.querySelector('.btn-clear');
 const btnFilter = document.querySelectorAll('.btn-filter');
-// const displayTodoList = document.querySelector('.list');
+const displayTodoList = document.querySelector('.list');
 const displayItemsLeft = document.querySelector('.items-left');
 
-// let todoList = [JSON.parse(localStorage.getItem('todoList'))];
-let todoList = [];
+let todoList = [JSON.parse(localStorage.getItem('todoList'))];
+// let todoList = [];
 
 // Toggle light and dark mode
 let theme = localStorage.getItem('theme');
@@ -34,168 +34,170 @@ function switchTheme() {
 
 if (theme === 'true') darkThemeOn();
 
-// // List all todos saved in local storage
-// function displayTodo(todoList = JSON.parse(localStorage.getItem('todoList'))) {
-//   if (todoList)
-//     todoList.forEach(todo => {
-//       const listItem = document.createElement('li');
+// List all todos saved in local storage
+function displayTodo(todoList = JSON.parse(localStorage.getItem('todoList'))) {
+  if (todoList)
+    todoList.forEach(todo => {
+      const listItem = document.createElement('li');
 
-//       listItem.classList.add('list__item');
-//       listItem.setAttribute('draggable', true);
-//       listItem.innerHTML = `
-//         <div class="checkbox-container">
-//           <input type="checkbox" class="btn btn-check"
-//             ${todo.isCompleted ? 'checked' : ''} />
-//           <span class="check"></span>
-//         </div>
-//         <p class="item ${todo.isCompleted ? 'completed' : ''}">${todo.todo}</p>
-//         <button class="btn btn-delete">DELETE</button>
-//       `;
+      listItem.classList.add('list__item');
+      listItem.setAttribute('draggable', true);
+      listItem.innerHTML = `
+        <div class="checkbox-container">
+          <input type="checkbox" id="completed" class="btn-checkbox
+            ${todo.isCompleted ? 'checked' : ''}" />
+          <label for="completed" class="btn-check--input">
+            <p class="paragraph paragraph-list 
+              ${todo.isCompleted ? 'completed' : ''}">${todo.todo}</p>
+          </label>
+        </div>
+        <button class="btn btn-delete"></button>
+      `;
 
-//       // Add event handlers to each todo
-//       listItem.addEventListener('dragstart', () => {
-//         listItem.classList.add('dragging');
-//       });
+      // Add event handlers to each todo
+      listItem.addEventListener('dragstart', () => {
+        listItem.classList.add('dragging');
+      });
 
-//       listItem.addEventListener('dragend', () => {
-//         listItem.classList.remove('dragging');
-//       });
+      listItem.addEventListener('dragend', () => {
+        listItem.classList.remove('dragging');
+      });
 
-//       listItem.querySelector('.btn-check').addEventListener('click', () => {
-//         const completed = listItem.querySelector('.btn-check').checked
-//           ? true
-//           : false;
+      listItem.querySelector('.btn-checkbox').addEventListener('click', () => {
+        const completed = listItem.querySelector('.btn-checkbox').checked
+          ? true
+          : false;
 
-//         completed
-//           ? listItem.querySelector('.item').classList.add('completed')
-//           : listItem.querySelector('.item').classList.remove('completed');
+        completed
+          ? listItem.querySelector('.paragraph').classList.add('completed')
+          : listItem.querySelector('.paragraph').classList.remove('completed');
 
-//         completedTodo(todo, completed);
-//       });
+        completedTodo(todo, completed);
+      });
 
-//       listItem.querySelector('.btn-delete').addEventListener('click', () => {
-//         displayTodoList.removeChild(listItem);
-//         deleteTodo(todo);
-//       });
+      listItem.querySelector('.btn-delete').addEventListener('click', () => {
+        displayTodoList.removeChild(listItem);
+        deleteTodo(todo);
+      });
 
-//       displayTodoList.appendChild(listItem);
-//     });
+      displayTodoList.appendChild(listItem);
+    });
 
-//   itemsLeft();
-//   dragItems();
-// }
+  itemsLeft();
+  dragItems();
+}
 
-// // Add new todo to the list
-// function addTodo() {
-//   const todo = newTodo.value.trim();
+// Add new todo to the list
+function addTodo() {
+  const todo = newTodo.value.trim();
 
-//   if (!todo) return;
+  if (!todo) return;
 
-//   const todoItem = {
-//     todo,
-//     isCompleted: false,
-//   };
+  const todoItem = {
+    todo,
+    isCompleted: false,
+  };
 
-//   todoList.push(todoItem);
-//   localStorage.setItem('todoList', JSON.stringify(todoList));
-//   displayTodo([todoItem]);
-//   itemsLeft();
-//   newTodo.value = '';
-//   newTodo.focus();
-// }
+  todoList.push(todoItem);
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+  displayTodo([todoItem]);
+  itemsLeft();
+  newTodo.value = '';
+  newTodo.focus();
+}
 
-// // Mark todos as complete
-// function completedTodo(todo, completed) {
-//   const index = todoList.findIndex(index => index.todo === todo.todo);
-//   todoList[index].isCompleted = completed;
-//   localStorage.setItem('todoList', JSON.stringify(todoList));
-//   itemsLeft();
-// }
+// Mark todos as complete
+function completedTodo(todo, completed) {
+  const index = todoList.findIndex(index => index.todo === todo.todo);
+  todoList[index].isCompleted = completed;
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+  itemsLeft();
+}
 
-// // Delete todos form the list
-// function deleteTodo(todo) {
-//   const index = todoList.findIndex(index => index.todo === todo.todo);
-//   todoList.splice(index, 1);
-//   localStorage.setItem('todoList', JSON.stringify(todoList));
-//   itemsLeft();
-// }
+// Delete todos form the list
+function deleteTodo(todo) {
+  const index = todoList.findIndex(index => index.todo === todo.todo);
+  todoList.splice(index, 1);
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+  itemsLeft();
+}
 
-// // Clear all completed todos
-// function deleteCompleted() {
-//   const completedItems = [...document.querySelectorAll('.completed')];
-//   completedItems.forEach(item => item.parentNode.remove(item));
+// Clear all completed todos
+function deleteCompleted() {
+  const completedItems = [...document.querySelectorAll('.completed')];
+  completedItems.forEach(item => item.parentNode.remove(item));
 
-//   todoList = JSON.parse(localStorage.getItem('todoList'));
-//   todoList = todoList.filter(todo => !todo.isCompleted);
-//   localStorage.setItem('todoList', JSON.stringify(todoList));
-// }
+  todoList = JSON.parse(localStorage.getItem('todoList'));
+  todoList = todoList.filter(todo => !todo.isCompleted);
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+}
 
-// // Drag and drop to reorder items on the list
-// function dragItems() {
-//   displayTodoList.addEventListener('dragover', e => {
-//     e.preventDefault();
+// Drag and drop to reorder items on the list
+function dragItems() {
+  displayTodoList.addEventListener('dragover', e => {
+    e.preventDefault();
 
-//     if (
-//       !e.target.classList.contains('dragging') &&
-//       e.target.classList.contains('item-list')
-//     ) {
-//       const draggingItem = document.querySelector('.dragging');
-//       const item = [...document.querySelectorAll('.item-list')];
-//       const currentPosition = item.indexOf(draggingItem);
-//       const newPosition = item.indexOf(e.target);
-//       const moved = todoList.splice(currentPosition, 1);
+    if (
+      !e.target.classList.contains('dragging') &&
+      e.target.classList.contains('item-list')
+    ) {
+      const draggingItem = document.querySelector('.dragging');
+      const item = [...document.querySelectorAll('.item-list')];
+      const currentPosition = item.indexOf(draggingItem);
+      const newPosition = item.indexOf(e.target);
+      const moved = todoList.splice(currentPosition, 1);
 
-//       currentPosition > newPosition
-//         ? displayTodoList.insertBefore(draggingItem, e.target)
-//         : displayTodoList.insertBefore(draggingItem, e.target.nextSibling);
+      currentPosition > newPosition
+        ? displayTodoList.insertBefore(draggingItem, e.target)
+        : displayTodoList.insertBefore(draggingItem, e.target.nextSibling);
 
-//       todoList.splice(newPosition, 0, moved[0]);
-//       localStorage.setItem('todoList', JSON.stringify(todoList));
-//     }
-//   });
-// }
+      todoList.splice(newPosition, 0, moved[0]);
+      localStorage.setItem('todoList', JSON.stringify(todoList));
+    }
+  });
+}
 
-// // Filter by all/active/complete todos
-// function filterView(itemType) {
-//   let completedItems = [...document.querySelectorAll('.item')];
-//   completedItems.forEach(item => {
-//     item.parentNode.classList.remove('hide');
-//   });
+// Filter by all/active/complete todos
+function filterView(itemType) {
+  let completedItems = [...document.querySelectorAll('.paragraph-list')];
+  completedItems.forEach(item => {
+    item.parentNode.classList.remove('hide');
+  });
 
-//   if (itemType) {
-//     completedItems = completedItems.filter(item =>
-//       itemType === 'completed'
-//         ? item.classList.contains('completed')
-//         : !item.classList.contains('completed'),
-//     );
+  if (itemType) {
+    completedItems = completedItems.filter(item =>
+      itemType === 'completed'
+        ? item.classList.contains('completed')
+        : !item.classList.contains('completed'),
+    );
 
-//     completedItems.forEach(item => {
-//       item.parentNode.classList.add('class', 'hide');
-//     });
-//   }
-// }
+    completedItems.forEach(item => {
+      item.parentNode.classList.add('class', 'hide');
+    });
+  }
+}
 
-// // Display left todos
-// function itemsLeft() {
-//   const itemsLength = [...document.querySelectorAll('.item')];
-//   displayItemsLeft.textContent = itemsLength.filter(
-//     item => !item.classList.contains('completed'),
-//   ).length;
-// }
+// Display left todos
+function itemsLeft() {
+  const itemsLength = [...document.querySelectorAll('.paragraph-list')];
+  displayItemsLeft.textContent = itemsLength.filter(
+    item => !item.classList.contains('completed'),
+  ).length;
+}
 
 function init() {
   // Event handlerss
-  // document.addEventListener('DOMContentLoaded', displayTodo());
-  // btnAdd.addEventListener('click', addTodo);
-  // btnClear.addEventListener('click', deleteCompleted);
+  document.addEventListener('DOMContentLoaded', displayTodo());
+  btnAdd.addEventListener('click', addTodo);
+  btnClear.addEventListener('click', deleteCompleted);
   themeSwitch.addEventListener('click', switchTheme);
-  // btnFilter.forEach(btn =>
-  //   btn.addEventListener('click', () => {
-  //     if (btn.classList.contains('btn-all')) filterView();
-  //     if (btn.classList.contains('btn-active')) filterView('completed');
-  //     if (btn.classList.contains('btn-completed')) filterView('not-completed');
-  //   }),
-  // );
+  btnFilter.forEach(btn =>
+    btn.addEventListener('click', () => {
+      if (btn.classList.contains('btn-all')) filterView();
+      if (btn.classList.contains('btn-active')) filterView('completed');
+      if (btn.classList.contains('btn-completed')) filterView('not-completed');
+    }),
+  );
 }
 
 init();
