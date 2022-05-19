99f554427274e7817acb97217acfcecf4d0edd81
wiksmf <wiksmf@gmail.com>
2022-05-19 21:52:00 +0200

    style  update

--- nft-preview-card-component-main/src/js/script.js
+++ nft-preview-card-component-main/src/js/script.js
@@ -1,11 +1,27 @@
 'use strict';
 
 const attributionManagement = () => {
-  const attribution = document.querySelector('.attribution-information');
+  // const attribution = document.querySelector('.attribution-information');
   const btnAttribution = document.querySelector('.attribution-btn');
+  const overlay = document.querySelector('.overlay');
 
   btnAttribution.addEventListener('click', () => {
-    attribution.classList.toggle('show');
+    btnAttribution.classList.toggle('show');
+    overlay.classList.toggle('hidden');
+  });
+
+  overlay.addEventListener('click', e => {
+    if (!overlay.classList.contains('hidden')) {
+      btnAttribution.classList.remove('show');
+      overlay.classList.add('hidden');
+    }
+  });
+
+  document.addEventListener('keydown', e => {
+    if (e.key === 'Escape' && btnAttribution.classList.contains('show')) {
+      btnAttribution.classList.remove('show');
+      overlay.classList.add('hidden');
+    }
   });
 };
 
