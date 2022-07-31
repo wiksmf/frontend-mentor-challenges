912258125e591adbf014d130dee0b178812d11ac
wiksmf <wiksmf@gmail.com>
2022-07-31 14:28:08 +0200

    initial commit

---
+++ social-proof-section-master/src/js/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+
+// Attribution view management
+const attributionManagement = () => {
+  const btnAttribution = document.querySelector('.attribution-btn');
+  const overlay = document.querySelector('.overlay');
+
+  btnAttribution.addEventListener('click', () => {
+    btnAttribution.classList.toggle('show');
+    overlay.classList.toggle('hidden');
+  });
+
+  overlay.addEventListener('click', e => {
+    if (!overlay.classList.contains('hidden')) {
+      btnAttribution.classList.remove('show');
+      overlay.classList.add('hidden');
+    }
+  });
+
+  document.addEventListener('keydown', e => {
+    if (e.key === 'Escape' && btnAttribution.classList.contains('show')) {
+      btnAttribution.classList.remove('show');
+      overlay.classList.add('hidden');
+    }
+  });
+};
+
+const init = () => {
+  // Page transition animation
+  window.addEventListener('load', () => {
+    const body= document.querySelector('body');
+    body.classList.add('open');
+  })
+
+  attributionManagement();
+};
+
+init();
