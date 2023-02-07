ee24bcd329206cd0d8f877c8f8e9bd765c5af69f
wiksmf <wiksmf@gmail.com>
2023-02-07 20:26:42 +0100

    routing update

--- planets-fact-site/src/app/app-routing.module.ts
+++ planets-fact-site/src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { PlanetComponent } from './planet/planet.component';
 
 const routes: Routes = [
-  {path: 'planet/:name', component: PlanetComponent},
-  {path: '', pathMatch: 'full', redirectTo: 'planet/earth'},
-  {path: '**', redirectTo: 'planet/earth'},
+  { path: 'planet/:name', component: PlanetComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'planet/earth' },
+  { path: '**', redirectTo: 'planet/earth' },
 ];
 
 @NgModule({
